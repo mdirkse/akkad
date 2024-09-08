7066d8ca0ab6af0ed564f71222efc1a21197f172
Maarten Dirkse <maarten.dirkse@gmail.com>
2024-09-08 22:32:22 +0200

    tmp

--- modules/system/module.nix
+++ modules/system/module.nix
@@ -1,28 +1,13 @@
 { inputs, lib, config, options, pkgs, ... }: {
-  # You can import other NixOS modules here
   imports = [
+    ./network.nix
     ./nix.nix
   ];
 
-  nixpkgs = { config = { allowUnfree = true; }; };
-
-  nix.registry = (lib.mapAttrs (_: flake: { inherit flake; }))
-    ((lib.filterAttrs (_: lib.isType "flake")) inputs);
-
-  nix.nixPath = [ "/etc/nix/path" ];
   environment.etc = lib.mapAttrs' (name: value: {
     name = "nix/path/${name}";
     value.source = value.flake;
   }) config.nix.registry;
-
-  nix.settings = {
-    experimental-features = "nix-command flakes";
-    auto-optimise-store = true;
-  };
-
-  networking.hostId = "bf663db0";
-  networking.hostName = "akkad";
-  networking.networkmanager.enable = true;
 
   services.openssh = {
     allowSFTP = false;
@@ -52,18 +37,8 @@
   hardware.bluetooth.powerOnBoot = true;
   services.blueman.enable = true;
 
-  networking.wireless.enable = false;
-
-  networking.timeServers = options.networking.timeServers.default ++ [ "0.nl.pool.ntp.org" "1.nl.pool.ntp.org" ];
-  networking. enableIPv6 = false;
-
   programs.command-not-found.enable = false;
   security.rtkit.enable = true;
-
-  networking.firewall = {
-    enable = true;
-    allowedTCPPorts = [ 22 443 1880 8123 ];
-  };
 
   time.timeZone = "Europe/Amsterdam";
 
@@ -89,7 +64,4 @@
       options = [ "SETENV" "NOPASSWD" ];
     }];
   }];
-
-  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion  # You can import other home-manager modules here
-  system.stateVersion = "23.11";
 }
--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -1,12 +1,14 @@
 { inputs, lib, config, options, pkgs, ... }: {
-  nix.nixPath = [ "/etc/nix/path" ];
+  documentation.man.enable = false;
 
-  nix.registry = (lib.mapAttrs (_: flake: { inherit flake; })) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
-
-  nix.settings = {
-    allowed-users = [ "@wheel" ];
-    auto-optimise-store = true;
-    experimental-features = "nix-command flakes";
+  nix = {
+    nixPath = [ "/etc/nix/path" ];
+    registry = (lib.mapAttrs (_: flake: { inherit flake; })) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
+    settings = {
+      allowed-users = [ "@wheel" ];
+      auto-optimise-store = true;
+      experimental-features = "nix-command flakes";
+    };
   };
 
   nixpkgs = { config = { allowUnfree = true; }; };
