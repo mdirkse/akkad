8edd50ed6a0232879dc0c28bb92b645f58aa6de8
Maarten Dirkse <maarten.dirkse@gmail.com>
2024-09-11 23:18:39 +0200

    tmp

--- modules/home-assistant/module.nix
+++ modules/home-assistant/module.nix
@@ -13,6 +13,11 @@
       # Includes dependencies for a basic setup
       # https://www.home-assistant.io/integrations/default_config/
       default_config = {};
+      http = {
+        server_host = "127.0.0.1";
+        trusted_proxies = "127.0.0.1";
+        use_x_forwarded_for = true;
+      };
     };
   };
 
