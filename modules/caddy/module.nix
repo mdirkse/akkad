8ed4db15845e7e4791faedf2fdc69522f5cce33f
Maarten Dirkse <maarten.dirkse@gmail.com>
2024-09-11 23:30:30 +0200

    tmp

--- modules/caddy/module.nix
+++ modules/caddy/module.nix
@@ -12,11 +12,21 @@
       '';
 
       virtualHosts."ha.${secrets.fqdn}".extraConfig = ''
+        log {
+          output file /var/log/caddy/ha.log {
+            roll_keep 0
+          }
+	      }
         reverse_proxy * localhost:8123
         tls /etc/ssl/${secrets.fqdn}.crt /etc/ssl/${secrets.fqdn}.key
       '';
 
       virtualHosts."nr.${secrets.fqdn}".extraConfig = ''
+        log {
+          output file /var/log/caddy/nr.log {
+            roll_keep 0
+          }
+	      }
         reverse_proxy * localhost:1880
         tls /etc/ssl/${secrets.fqdn}.crt /etc/ssl/${secrets.fqdn}.key
       '';
