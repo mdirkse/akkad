ef249f9296344f18604bdeb9a89edfe0da4d41c4
Maarten Dirkse <maarten.dirkse@gmail.com>
2024-09-12 10:19:51 +0200

    tmp

--- modules/caddy/module.nix
+++ modules/caddy/module.nix
@@ -11,34 +11,41 @@
         auto_https off
       '';
 
-      virtualHosts."akkad.${secrets.fqdn}".extraConfig = ''
-        log {
-          output file /var/log/caddy/ha.log {
+      virtualHosts."akkad.${secrets.fqdn}" = {
+        logFormat = ''
+          output file ${config.services.caddy.logDir}/access-akkad.log {
             roll_keep 0
           }
-	      }
-        respond 418
-      '';
+        '';
+        extraConfig = ''
+          respond 418
+          tls /etc/ssl/${secrets.fqdn}.crt /etc/ssl/${secrets.fqdn}.key
+        '';
+      };
 
-      virtualHosts."ha.${secrets.fqdn}".extraConfig = ''
-        log {
-          output file /var/log/caddy/ha.log {
+      virtualHosts."ha.${secrets.fqdn}" = {
+        logFormat = ''
+          output file ${config.services.caddy.logDir}/access-ha.log {
             roll_keep 0
           }
-	      }
-        reverse_proxy * localhost:8123
-        tls /etc/ssl/${secrets.fqdn}.crt /etc/ssl/${secrets.fqdn}.key
-      '';
+        '';
+        extraConfig = ''
+          reverse_proxy * localhost:8123
+          tls /etc/ssl/${secrets.fqdn}.crt /etc/ssl/${secrets.fqdn}.key
+        '';
+      };
 
-      virtualHosts."nr.${secrets.fqdn}".extraConfig = ''
-        log {
-          output file /var/log/caddy/nr.log {
+      virtualHosts."nr.${secrets.fqdn}" = {
+        logFormat = ''
+          output file ${config.services.caddy.logDir}/access-nr.log {
             roll_keep 0
           }
-	      }
-        reverse_proxy * localhost:1880
-        tls /etc/ssl/${secrets.fqdn}.crt /etc/ssl/${secrets.fqdn}.key
-      '';
+        '';
+        extraConfig = ''
+          reverse_proxy * localhost:1880
+          tls /etc/ssl/${secrets.fqdn}.crt /etc/ssl/${secrets.fqdn}.key
+        '';
     };
-  }
+  };
+}
 
