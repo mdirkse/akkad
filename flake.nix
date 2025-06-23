d5f1b226a995ebbced8d28cfe92f439bab2ad44d
Maarten Dirkse <maarten.dirkse@gmail.com>
2025-06-23 11:13:04 +0200

    Upgrade to 25.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Home Assistant Nix config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   };
 
   outputs = { self, nixpkgs, ... }@inputs:
